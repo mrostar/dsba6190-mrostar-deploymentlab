0ea25c1706f315ba0b2a78c8c65a464ee44d0357
mrostar <mrostar2002@gmail.com>
2025-11-30 20:18:12 -0500

    update main.tf

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "azurerm_subnet" "snet" {
 // SQL Server
 
 resource "azurerm_mssql_server" "sql" {
-  name                         = "mrostar-server"
+  name                         = "mrostar-sqlserver"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
