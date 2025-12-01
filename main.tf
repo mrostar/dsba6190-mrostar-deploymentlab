2003f56122a7aa999ff003fe7640017160588a11
mrostar <mrostar2002@gmail.com>
2025-11-30 19:19:10 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,15 +40,13 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-    network_rules {
+  network_rules {
     default_action             = "Deny"
     ip_rules                   = ["100.0.0.1"]
     virtual_network_subnet_ids = [azurerm_subnet.snet.id]
   }
 
-
   tags = local.tags
-
   
   is_hns_enabled = true
 
