d93d08a2b5947fd982516cd34efbc3a51ebac32a
mrostar <mrostar2002@gmail.com>
2025-11-30 19:12:22 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,5 +40,78 @@ resource "azurerm_storage_account" "storage" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
+    network_rules {
+    default_action             = "Deny"
+    ip_rules                   = ["100.0.0.1"]
+    virtual_network_subnet_ids = [azurerm_subnet.snet.id]
+  }
+
+
   tags = local.tags
+
+  
+  is_hns_enabled = true
+
+}
+
+
+
+// Virtual Network
+
+resource "azurerm_virtual_network" "virtualnet" {
+  name                = "virtualnet-${var.class_name}-${var.student_name}-${var.environment}-${var.location}-${random_integer.deployment_id_suffix.result}"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+// subnet
+
+resource "azurerm_subnet" "snet" {
+  name                 = "snetmrostar"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.virtualnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+  service_endpoints    = ["Microsoft.Sql", "Microsoft.Storage"]
+}
+
+
+
+// SQL Server
+
+resource "azurerm_mssql_server" "sql" {
+  name                         = "sql-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+// SQL DB
+
+resource "azurerm_mssql_database" "db" {
+  name         = "db-${var.class_name}${var.student_name}${var.environment}${random_integer.deployment_id_suffix.result}"
+  server_id    = azurerm_mssql_server.sql.id
+  collation    = "SQL_Latin1_General_CP1_CI_AS"
+  license_type = "LicenseIncluded"
+  max_size_gb  = 2
+  sku_name     = "S0"
+  enclave_type = "VBS"
+
+  tags = {
+    foo = "bar"
+  }
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+// virtual network rule
+
+resource "azurerm_mssql_virtual_network_rule" "virtualnetwork-rule" {
+  name      = "mrostar_rule"
+  server_id = azurerm_mssql_server.sql.id
+  subnet_id = azurerm_subnet.snet.id
 }
